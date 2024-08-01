85dafb1ca69b9039b40a701e97013ae51112e7d4
alex-belarus <am_stelm@mail.ru>
2024-08-01 21:27:50 +0300

    6

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drf_yasg',
+    'django-dump-load-utf8',
     'debug_toolbar',
     'rest_framework',
     'main',
